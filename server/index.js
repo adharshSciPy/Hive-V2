bf7e41d3285a6f53b9251724e9f0659b7ea18c69
adharsh <adharsh@maptell.com>
2024-02-21 12:04:12 +0530

    new server included

--- server/index.js
+++ server/index.js
@@ -1,15 +1,60 @@
-import dotenv from 'dotenv';
-import { app } from './app.js'
-import connectMongoDb from './src/config/mongodb.js';
+const express = require("express");
+const app = express();
+const User = require('./models/userSchema')
+const socket = require('socket.io')
+const PORT = 5000;
+const connect = require("./mongodb/config.js");
+const userRouter = require("./routers/UserRouter.js");
+const publicRouter = require("./routers/PublicRouter.js");
+const adminRouter = require("./routers/adminRouter.js")
+const studentRouter = require('./routers/studentRouter.js')
+const chatRoute = require('./routers/chatRoute.js')
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require('path');
 
-dotenv.config({
-    path: './.env'
+// mongoose connection
+connect();
+
+// middlewares
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+// app.use('/public', express.static(__dirname + '/uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.use("/user", userRouter);
+app.use("/public", publicRouter);
+app.use('/admin', adminRouter);
+app.use('/student', studentRouter);
+app.use('/chat', chatRoute)
+
+// Start the server
+const server = app.listen(5000, () => {
+  console.log('Server listening on port 5000');
+});
+
+// chat socket.io logic
+const io = socket(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    credentials: true
+  }
 })
 
-const PORT = process.env.PORT;
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("addUser", (id) => {
+    onlineUsers.set(id, socket.id);
+  })
 
-connectMongoDb()
-    .then(() => {
-        app.listen(PORT, () => console.log(`server connected @ ${PORT}`))
-    })
-    .catch((err) => console.log(`server faied ${err}`))
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-receive", data.message)
+    }
+  })
+})
+
--- server/src/routers/public.routes.js
+++
@@ -1,9 +0,0 @@
-import Router from 'express'
-import { upload } from '../config/multer.js'
-import { createClass } from '../controllers/public.controller.js'
-
-const publicRouter = Router()
-
-publicRouter.route('/create-class/:userId', upload.single('file')).post(createClass)
-
-export default publicRouter  
